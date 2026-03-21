cd3b227e21ed2d7d073e5a3a6d38ae7ed757f36c
locusts32 <foxri0403@gmail.com>
2026-03-22 02:21:17 +0900

    fix pakage.json

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ app.get("/alerts/check", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`서버 실행 중: http://localhost:${PORT}`);
+    console.log(`서버 실행 중: ${PORT}`);
 });
